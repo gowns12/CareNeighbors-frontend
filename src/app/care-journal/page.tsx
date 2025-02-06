4a2c1bdc5243e418b5013c6e37cd0314a4b27781
booungyi <ansdlsgur0123@gmail.com>
2025-02-06 18:08:16 +0900

    버튼 구현중

--- src/app/care-journal/page.tsx
+++ src/app/care-journal/page.tsx
@@ -1,11 +1,25 @@
-// src/app/care-journal/page.tsx
 import React from "react";
+import styles from "./page.module.css";
 
 export default function Page() {
-  return (
-    <div>
-      <h2>Care Journal</h2>
-      <p>환자의 치료 일지를 관리하세요.</p>
-    </div>
-  );
+
+    return (
+        <div className={styles.outerContainer}>
+            <div className={styles.innerContainer}>
+
+                <header className={styles.header}>
+                    <button className={styles.button}>메뉴</button>
+                    <h2 className={styles.title}>간병일지</h2>
+                    <button className={styles.button}> 달력전환</button>
+                </header>
+                <div style={{border: "1px solid red"}}>
+                    fff
+                </div>
+                <main style={{border: "1px solid blue"}} className={styles.main}>
+                    dfsgsdgdsfg
+                </main>
+                <footer className={styles.footer}>fsadfdsafsaf</footer>
+            </div>
+        </div>
+    );
 }
